564243b2c6009fee7b381d66003b5213346a41a1
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-04-16 15:24:26 -0400

    add style navbar and sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,14 @@
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="h-[calc(100vh-2rem)] w-full max-w-[15rem] p-4 shadow-xl shadow-blue-gray-900/5">
+      <ul className="my-5 flex flex-col gap-2">
+        <li>Categories</li>
+        <li>
+          <input type="text" placeholder="Search" />
+        </li>
+        <li>Price range</li>
+      </ul>
+    </div>
+  );
 };
 export default SideBar;
