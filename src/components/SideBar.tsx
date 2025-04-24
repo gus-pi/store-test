e7fb94747a5e2a4811aff14554afaa8be8d41596
PieMenard <pinedo.m.gustavo@gmail.com>
2025-04-24 01:26:00 -0400

    style filter input

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchCategories } from '../services/categoryServices';
 import { Category, Filter } from '../lib/types';
 
@@ -52,14 +52,31 @@ const SideBar = ({
           </select>
         )}
         <li>
-          <input
-            className="px-2"
-            type="text"
-            placeholder="Filter by title"
-            onChange={(e) =>
-              setActiveFilter({ ...activeFilter, title: e.target.value })
-            }
-          />
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input
+              type="search"
+              placeholder="Filter by title"
+              onChange={(e) =>
+                setActiveFilter({ ...activeFilter, title: e.target.value })
+              }
+            />
+          </label>
         </li>
         <li>Price range</li>
       </ul>
